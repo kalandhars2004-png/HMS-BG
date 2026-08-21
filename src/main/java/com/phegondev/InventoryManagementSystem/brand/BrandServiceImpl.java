6a4b030f9e064abd68eef77e16dd4506cf6c0f44
kalandhar01 <kalandars2004@gmail.com>
2026-08-21 20:58:30 +0530

    fix: set brand createdAt on create

--- src/main/java/com/phegondev/InventoryManagementSystem/brand/BrandServiceImpl.java
+++ src/main/java/com/phegondev/InventoryManagementSystem/brand/BrandServiceImpl.java
@@ -14,6 +14,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -27,6 +28,7 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public Response createBrand(BrandDTO brandDTO) {
         Brand brandToSave = modelMapper.map(brandDTO, Brand.class);
+        brandToSave.setCreatedAt(LocalDateTime.now());
         brandRepository.save(brandToSave);
 
         return Response.builder()
